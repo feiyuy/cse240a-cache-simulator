7499dab16761b06f2de3448db7cfc545e75c388f
Feiyu Yang <yangfeiyu@Feiyus-Macbook-Pro.local>
2022-05-30 16:52:44 -0700

    Finish coding.

--- src/cache.c
+++ src/cache.c
@@ -11,9 +11,9 @@
 //
 // TODO:Student Information
 //
-const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentName = "Feiyu Yang";
+const char *studentID   = "A59009686";
+const char *email       = "f8yang@ucsd.edu";
 
 //------------------------------------//
 //        Cache Configuration         //
@@ -54,11 +54,17 @@ uint64_t l2cachePenalties; // L2$ penalties
 //------------------------------------//
 //        Cache Data Structures       //
 //------------------------------------//
-
+#define MAX_SETS 16384
+#define MAX_ASSOS 16
 //
 //TODO: Add your Cache data structures here
-//
+uint32_t icache[MAX_SETS][MAX_ASSOS];
+uint32_t dcache[MAX_SETS][MAX_ASSOS];
+uint32_t l2cache[MAX_SETS][MAX_ASSOS];
 
+uint32_t icacheLRU[MAX_SETS][MAX_ASSOS];
+uint32_t dcacheLRU[MAX_SETS][MAX_ASSOS];
+uint32_t l2cacheLRU[MAX_SETS][MAX_ASSOS];
 //------------------------------------//
 //          Cache Functions           //
 //------------------------------------//
@@ -82,6 +88,17 @@ init_cache()
   //
   //TODO: Initialize Cache Simulator Data Structures
   //
+  for (int i=0; i<MAX_SETS; i++){
+    for (int j=0; j<MAX_ASSOS; j++){
+      icache[i][j] = 0;
+      dcache[i][j] = 0;
+      l2cache[i][j] = 0;
+
+      icacheLRU[i][j] = j;
+      dcacheLRU[i][j] = j;
+      l2cacheLRU[i][j] = j;
+    }
+  }
 }
 
 // Perform a memory access through the icache interface for the address 'addr'
@@ -93,7 +110,47 @@ icache_access(uint32_t addr)
   //
   //TODO: Implement I$
   //
-  return memspeed;
+  uint32_t speed = 0;
+
+  uint32_t set_index = (addr >> log2(blocksize)) & (icacheSets-1);
+  uint32_t tag = addr >> log2(blocksize*icacheSets);
+
+  int flag =  0;
+
+  for (int i=0; i<icacheAssoc; i++){
+    if (icache[set_index][i] == tag){
+      speed = icacheHitTime;
+      icacheRefs++;
+      flag = 1;
+
+      for (int j=0; j<icacheAssoc; j++){
+        if (icacheLRU[set_index][j] == i){
+          for (int k=j; k<icacheAssoc-1; k++){
+            icacheLRU[set_index][k] = icacheLRU[set_index][k+1];
+          }
+          icacheLRU[set_index][icacheAssoc-1] = i;
+          break;
+        }
+      }
+
+      break;
+    }
+  }
+
+  if (flag == 0){
+    speed = icacheHitTime + l2cache_access(addr);
+    icacheMisses++;
+    icachePenalties += speed;
+    
+    uint32_t currentLRU = icacheLRU[set_index][0];
+    icacheLRU[set_index][currentLRU] = tag;
+    for (int k=0; k<icacheAssoc-1; k++){
+      icacheLRU[set_index][k] = icacheLRU[set_index][k+1];
+    }
+    icacheLRU[set_index][icacheAssoc-1] = currentLRU;
+  }
+
+  return speed;
 }
 
 // Perform a memory access through the dcache interface for the address 'addr'
@@ -105,7 +162,47 @@ dcache_access(uint32_t addr)
   //
   //TODO: Implement D$
   //
-  return memspeed;
+  uint32_t speed = 0;
+
+  uint32_t set_index = (addr >> log2(blocksize)) & (dcacheSets-1);
+  uint32_t tag = addr >> log2(blocksize*dcacheSets);
+
+  int flag =  0;
+
+  for (int i=0; i<dcacheAssoc; i++){
+    if (dcache[set_index][i] == tag){
+      speed = dcacheHitTime;
+      dcacheRefs++;
+      flag = 1;
+
+      for (int j=0; j<dcacheAssoc; j++){
+        if (dcacheLRU[set_index][j] == i){
+          for (int k=j; k<dcacheAssoc-1; k++){
+            dcacheLRU[set_index][k] = dcacheLRU[set_index][k+1];
+          }
+          dcacheLRU[set_index][dcacheAssoc-1] = i;
+          break;
+        }
+      }
+
+      break;
+    }
+  }
+
+  if (flag == 0){
+    speed = dcacheHitTime + l2cache_access(addr);
+    dcacheMisses++;
+    dcachePenalties += speed;
+    
+    uint32_t currentLRU = dcacheLRU[set_index][0];
+    dcacheLRU[set_index][currentLRU] = tag;
+    for (int k=0; k<dcacheAssoc-1; k++){
+      dcacheLRU[set_index][k] = dcacheLRU[set_index][k+1];
+    }
+    dcacheLRU[set_index][dcacheAssoc-1] = currentLRU;
+  }
+
+  return speed;
 }
 
 // Perform a memory access to the l2cache for the address 'addr'
@@ -117,5 +214,45 @@ l2cache_access(uint32_t addr)
   //
   //TODO: Implement L2$
   //
-  return memspeed;
+  uint32_t speed = 0;
+
+  uint32_t set_index = (addr >> log2(blocksize)) & (l2cacheSets-1);
+  uint32_t tag = addr >> log2(blocksize*l2cacheSets);
+
+  int flag =  0;
+
+  for (int i=0; i<l2cacheAssoc; i++){
+    if (l2cache[set_index][i] == tag){
+      speed = l2cacheHitTime;
+      l2cacheRefs++;
+      flag = 1;
+
+      for (int j=0; j<l2cacheAssoc; j++){
+        if (l2cacheLRU[set_index][j] == i){
+          for (int k=j; k<l2cacheAssoc-1; k++){
+            l2cacheLRU[set_index][k] = l2cacheLRU[set_index][k+1];
+          }
+          l2cacheLRU[set_index][l2cacheAssoc-1] = i;
+          break;
+        }
+      }
+
+      break;
+    }
+  }
+
+  if (flag == 0){
+    speed = l2cacheHitTime + memspeed;
+    l2cacheMisses++;
+    l2cachePenalties += speed;
+    
+    uint32_t currentLRU = l2cacheLRU[set_index][0];
+    l2cacheLRU[set_index][currentLRU] = tag;
+    for (int k=0; k<l2cacheAssoc-1; k++){
+      l2cacheLRU[set_index][k] = l2cacheLRU[set_index][k+1];
+    }
+    l2cacheLRU[set_index][l2cacheAssoc-1] = currentLRU;
+  }
+
+  return speed;
 }
